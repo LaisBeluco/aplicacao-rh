230a5bc17c2b9a63f2689c7d14f819ad89698ecc
Lais B <laisbelucom@gmail.com>
2025-01-17 20:26:53 -0300

    ARG-main - Partes de configuração do projeto

---
+++ laravel-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show()
+    {
+        dd("pau no cu");
+    }
+}
