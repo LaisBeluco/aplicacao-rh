81cbed03ab4ab12850f5e7d481c2d338aed4f2d4
Lais B <laisbelucom@gmail.com>
2025-01-23 22:44:35 -0300

    ARH-main - Implementação de mask e feito cadastro base dos colaboradores

--- api/app/Http/Requests/EmployeeRequest.php
+++ api/app/Http/Requests/EmployeeRequest.php
@@ -19,7 +19,7 @@ class EmployeeRequest extends ApiFormRequest
             'code' => 'required|max:10',
             'full_name' => 'required|max:255',
             'user_name' => 'required|max:55',
-            'document' => 'required|min:11|max:11|unique:employees,document,' . $this->id,
+            'document' => 'required|min:11|max:11|unique:employees,document,' . ( $this->id ?? "NULL"),
             'date_birth' => 'required|date',
             'mother_name' => 'required|max:255',
             'father_name' => 'max:255',
--- api/database/migrations/2025_01_19_014105_create_employees_table.php
+++ api/database/migrations/2025_01_19_014105_create_employees_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('code');
             $table->string('full_name');
             $table->string('user_name');
